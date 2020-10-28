78b78dc5c15550438c4a0c91befe68a09363306e
devtfq <69789507+devtfq@users.noreply.github.com>
2020-10-28 23:44:38 +0530

    removed trig functions
    So that i can make my own custom implementations ;)

--- src/main/java/net/objecthunter/exp4j/function/Functions.java
+++ src/main/java/net/objecthunter/exp4j/function/Functions.java
@@ -19,70 +19,28 @@ package net.objecthunter.exp4j.function;
  * Class representing the builtin functions available for use in expressions
  */
 public class Functions {
+    
+    private static final int INDEX_SQRT = 0;
+    private static final int INDEX_CBRT = 1;
+    private static final int INDEX_ABS = 2;
+    private static final int INDEX_CEIL = 3;
+    private static final int INDEX_FLOOR = 4;
+    private static final int INDEX_POW = 5;
+    private static final int INDEX_EXP = 6;
+    private static final int INDEX_EXPM1 =7;
+    private static final int INDEX_LOG10 = 8;
+    private static final int INDEX_LOG2 = 9;
+    private static final int INDEX_LOG = 10;
+    private static final int INDEX_LOG1P = 11;
+    private static final int INDEX_LOGB = 12;
+    private static final int INDEX_SGN = 13;
+    private static final int INDEX_TO_RADIAN = 14;
+    private static final int INDEX_TO_DEGREE = 15;
 
-    private static final int INDEX_SIN = 0;
-    private static final int INDEX_COS = 1;
-    private static final int INDEX_TAN = 2;
-    private static final int INDEX_CSC = 3;
-    private static final int INDEX_SEC = 4;
-    private static final int INDEX_COT = 5;
-    private static final int INDEX_SINH = 6;
-    private static final int INDEX_COSH = 7;
-    private static final int INDEX_TANH = 8;
-    private static final int INDEX_CSCH = 9;
-    private static final int INDEX_SECH = 10;
-    private static final int INDEX_COTH = 11;
-    private static final int INDEX_ASIN = 12;
-    private static final int INDEX_ACOS = 13;
-    private static final int INDEX_ATAN = 14;
-    private static final int INDEX_SQRT = 15;
-    private static final int INDEX_CBRT = 16;
-    private static final int INDEX_ABS = 17;
-    private static final int INDEX_CEIL = 18;
-    private static final int INDEX_FLOOR = 19;
-    private static final int INDEX_POW = 20;
-    private static final int INDEX_EXP = 21;
-    private static final int INDEX_EXPM1 = 22;
-    private static final int INDEX_LOG10 = 23;
-    private static final int INDEX_LOG2 = 24;
-    private static final int INDEX_LOG = 25;
-    private static final int INDEX_LOG1P = 26;
-    private static final int INDEX_LOGB = 27;
-    private static final int INDEX_SGN = 28;
-    private static final int INDEX_TO_RADIAN = 29;
-    private static final int INDEX_TO_DEGREE = 30;
-
-    private static final Function[] BUILT_IN_FUNCTIONS = new Function[31];
+    private static final Function[] BUILT_IN_FUNCTIONS = new Function[16];
 
     static {
-        BUILT_IN_FUNCTIONS[INDEX_SIN] = new Function("sin") {
-            @Override
-            public double apply(double... args) {
-                return Math.sin(args[0]);
-            }
-        };
-        BUILT_IN_FUNCTIONS[INDEX_COS] = new Function("cos") {
-            @Override
-            public double apply(double... args) {
-                return Math.cos(args[0]);
-            }
-        };
-        BUILT_IN_FUNCTIONS[INDEX_TAN] = new Function("tan") {
-            @Override
-            public double apply(double... args) {
-                return Math.tan(args[0]);
-            }
-        };
-        BUILT_IN_FUNCTIONS[INDEX_COT] = new Function("cot") {
-            @Override
-            public double apply(double... args) {
-                double tan = Math.tan(args[0]);
-                if (tan == 0d) {
-                    throw new ArithmeticException("Division by zero in cotangent!");
-                }
-                return 1d / tan;
-            }
-        };
+        
         BUILT_IN_FUNCTIONS[INDEX_LOG] = new Function("log") {
             @Override
             public double apply(double... args) {
@@ -113,24 +71,6 @@ public class Functions {
                 return Math.abs(args[0]);
             }
         };
-        BUILT_IN_FUNCTIONS[INDEX_ACOS] = new Function("acos") {
-            @Override
-            public double apply(double... args) {
-                return Math.acos(args[0]);
-            }
-        };
-        BUILT_IN_FUNCTIONS[INDEX_ASIN] = new Function("asin") {
-            @Override
-            public double apply(double... args) {
-                return Math.asin(args[0]);
-            }
-        };
-        BUILT_IN_FUNCTIONS[INDEX_ATAN] = new Function("atan") {
-            @Override
-            public double apply(double... args) {
-                return Math.atan(args[0]);
-            }
-        };
         BUILT_IN_FUNCTIONS[INDEX_CBRT] = new Function("cbrt") {
             @Override
             public double apply(double... args) {
@@ -143,31 +83,13 @@ public class Functions {
                 return Math.floor(args[0]);
             }
         };
-        BUILT_IN_FUNCTIONS[INDEX_SINH] = new Function("sinh") {
-            @Override
-            public double apply(double... args) {
-                return Math.sinh(args[0]);
-            }
-        };
         BUILT_IN_FUNCTIONS[INDEX_SQRT] = new Function("sqrt") {
             @Override
             public double apply(double... args) {
                 return Math.sqrt(args[0]);
             }
         };
-        BUILT_IN_FUNCTIONS[INDEX_TANH] = new Function("tanh") {
-            @Override
-            public double apply(double... args) {
-                return Math.tanh(args[0]);
-            }
-        };
-        BUILT_IN_FUNCTIONS[INDEX_COSH] = new Function("cosh") {
-            @Override
-            public double apply(double... args) {
-                return Math.cosh(args[0]);
-            }
-        };
-        BUILT_IN_FUNCTIONS[INDEX_CEIL] = new Function("ceil") {
+       BUILT_IN_FUNCTIONS[INDEX_CEIL] = new Function("ceil") {
             @Override
             public double apply(double... args) {
                 return Math.ceil(args[0]);
@@ -203,49 +125,6 @@ public class Functions {
                 }
             }
         };
-        BUILT_IN_FUNCTIONS[INDEX_CSC] = new Function("csc") {
-            @Override
-            public double apply(double... args) {
-                double sin = Math.sin(args[0]);
-                if (sin == 0d) {
-                    throw new ArithmeticException("Division by zero in cosecant!");
-                }
-                return 1d / sin;
-            }
-        };
-        BUILT_IN_FUNCTIONS[INDEX_SEC] = new Function("sec") {
-            @Override
-            public double apply(double... args) {
-                double cos = Math.cos(args[0]);
-                if (cos == 0d) {
-                    throw new ArithmeticException("Division by zero in secant!");
-                }
-                return 1d / cos;
-            }
-        };
-        BUILT_IN_FUNCTIONS[INDEX_CSCH] = new Function("csch") {
-            @Override
-            public double apply(double... args) {
-                //this would throw an ArithmeticException later as sinh(0) = 0
-                if (args[0] == 0d) {
-                    return 0;
-                }
-
-                return 1d / Math.sinh(args[0]);
-            }
-        };
-        BUILT_IN_FUNCTIONS[INDEX_SECH] = new Function("sech") {
-            @Override
-            public double apply(double... args) {
-                return 1d / Math.cosh(args[0]);
-            }
-        };
-        BUILT_IN_FUNCTIONS[INDEX_COTH] = new Function("coth") {
-            @Override
-            public double apply(double... args) {
-                return Math.cosh(args[0]) / Math.sinh(args[0]);
-            }
-        };
         BUILT_IN_FUNCTIONS[INDEX_LOGB] = new Function("logb", 2) {
             @Override
             public double apply(double... args) {
@@ -276,26 +155,6 @@ public class Functions {
     public static Function getBuiltinFunction(final String name) {
 
         switch (name) {
-            case "sin":
-                return BUILT_IN_FUNCTIONS[INDEX_SIN];
-            case "cos":
-                return BUILT_IN_FUNCTIONS[INDEX_COS];
-            case "tan":
-                return BUILT_IN_FUNCTIONS[INDEX_TAN];
-            case "cot":
-                return BUILT_IN_FUNCTIONS[INDEX_COT];
-            case "asin":
-                return BUILT_IN_FUNCTIONS[INDEX_ASIN];
-            case "acos":
-                return BUILT_IN_FUNCTIONS[INDEX_ACOS];
-            case "atan":
-                return BUILT_IN_FUNCTIONS[INDEX_ATAN];
-            case "sinh":
-                return BUILT_IN_FUNCTIONS[INDEX_SINH];
-            case "cosh":
-                return BUILT_IN_FUNCTIONS[INDEX_COSH];
-            case "tanh":
-                return BUILT_IN_FUNCTIONS[INDEX_TANH];
             case "abs":
                 return BUILT_IN_FUNCTIONS[INDEX_ABS];
             case "log":
@@ -322,16 +181,6 @@ public class Functions {
                 return BUILT_IN_FUNCTIONS[INDEX_EXPM1];
             case "signum":
                 return BUILT_IN_FUNCTIONS[INDEX_SGN];
-            case "csc":
-                return BUILT_IN_FUNCTIONS[INDEX_CSC];
-            case "sec":
-                return BUILT_IN_FUNCTIONS[INDEX_SEC];
-            case "csch":
-                return BUILT_IN_FUNCTIONS[INDEX_CSCH];
-            case "sech":
-                return BUILT_IN_FUNCTIONS[INDEX_SECH];
-            case "coth":
-                return BUILT_IN_FUNCTIONS[INDEX_COTH];
             case "toradian":
                 return BUILT_IN_FUNCTIONS[INDEX_TO_RADIAN];
             case "todegree":
